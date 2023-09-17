fe2be8dc117aded1369e666b452185e02d8f4b73
BoyOrigin <ahmadyasinfikri@gmail.com>
2023-09-18 01:37:44 +0700

    Fix Window size on unset fullscreen

--- src/wl_window.c
+++ src/wl_window.c
@@ -2615,6 +2615,8 @@ void _glfwSetWindowSizeWayland(_GLFWwindow* window, int width, int height)
             libdecor_state_free(frameState);
         }
 
+        _glfwInputWindowSize(window, width, height);
+
         if (window->wl.visible)
             _glfwInputWindowDamage(window);
     }
